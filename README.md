f7c93dfef57d12336af6478ca2898dad012174ef
NeonOrbit <66206913+NeonOrbit@users.noreply.github.com>
2021-06-14 06:21:19 +0600

    Initial Release

---
+++ README.md
@@ -0,0 +1,18 @@
+ChatHeadEnabler
+===============
+
+Xposed module to enbale Facebook Messenger chat head.
+
+
+Installation
+------------
+
+* Install [Magisk](https://www.xda-developers.com/how-to-install-magisk/)
+* Install, and activate [LSPosed](https://github.com/LSPosed/LSPosed) or [EdXposed](https://github.com/ElderDrivers/EdXposed)
+* Download, install, and activate [ChatHeadEnabler](https://github.com/NeonOrbit/ChatHeadEnabler/releases)
+
+
+Certificate fingerprints:
+
+* SHA1: FE:20:18:3C:7D:2F:5C:5D:9F:E1:BC:E6:B7:AB:31:A3:5F:F4:C8:D0
+* SHA256: 91:87:03:31:A4:5A:1C:1E:8F:34:BB:27:D6:97:3C:D7:2F:D8:AD:98:CB:F4:B3:06:27:6B:FE:C0:6D:61:EB:E8
---
+++ app/src/main/java/app/neonorbit/chatheadenabler/ModuleEntryPoint.java
@@ -0,0 +1,28 @@
+package app.neonorbit.chatheadenabler;
+
+import android.os.Build;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.setStaticIntField;
+
+public class ModuleEntryPoint implements IXposedHookLoadPackage {
+
+  private static final String   MODULE_NAME     = "ChatHeadEnabler";
+  private static final String   TARGET_PACKAGE  = "com.facebook.orca";
+  private static final int      SPOOF_VERSION   = Build.VERSION_CODES.Q;
+
+  @Override
+  public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+
+    if (!lpparam.packageName.equals(TARGET_PACKAGE))
+      return;
+
+    XposedBridge.log("Applying " + MODULE_NAME + " module to " + lpparam.packageName);
+
+    setStaticIntField(Build.VERSION.class, "SDK_INT", SPOOF_VERSION);
+
+  }
+}
